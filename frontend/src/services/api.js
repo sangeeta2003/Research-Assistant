befe46ba12b4dc5b3d2b224ffe6e62b47b700054
sangeeta2003 <mishrasangeeta944@gmail.com>
2026-07-25 19:59:21 +0530

    updat epai.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://research-assistant-rj4p.onrender.com/",
+  baseURL: "https://research-assistant-rj4p.onrender.com/api",
 });
 
 export default api;
