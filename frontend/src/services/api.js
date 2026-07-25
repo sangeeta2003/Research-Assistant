dac7599672ade675f27ae0cced683b62f5b361bc
sangeeta2003 <mishrasangeeta944@gmail.com>
2026-07-25 19:55:44 +0530

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://research-assistant.onrender.com/api",
+  baseURL: "https://research-assistant-rj4p.onrender.com/",
 });
 
 export default api;
